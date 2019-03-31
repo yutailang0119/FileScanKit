6ded257461dfa54cf89fefed3f416ba976b856ed
yutailang0119 <muta.yutaro@gmail.com>
2019-04-01 00:39:07 +0900

    Introduce PathKit via SwiftPM

--- Package.swift
+++ Package.swift
@@ -6,21 +6,17 @@ import PackageDescription
 let package = Package(
     name: "FileScanKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FileScanKit",
             targets: ["FileScanKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FileScanKit",
-            dependencies: []),
+            dependencies: ["PathKit"]),
         .testTarget(
             name: "FileScanKitTests",
             dependencies: ["FileScanKit"]),
